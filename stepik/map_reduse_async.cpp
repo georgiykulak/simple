78b962da4e163719969870a1c85ac6e82bd8d9ac
Georgiy Kulak <kulakgeorgij@gmail.com>
2021-01-29 18:11:22 +0200

    Stepik: Create not async solution for map_reduce

--- stepik/map_reduse_async.cpp
+++ stepik/map_reduse_async.cpp
@@ -8,7 +8,30 @@ template< typename BIterator, typename UnaryF, typename BinaryF >
 auto map_reduce( BIterator p, BIterator q, UnaryF f1, BinaryF f2, size_t threads )
     -> decltype( std::declval< BinaryF& >()( *std::declval< BIterator >(), *std::declval< BIterator >() ) )
 {
-    return {};
+    auto const length = std::distance( p, q );
+    std::size_t const cnt_per_thrd = length / threads;
+
+    /*auto res = f1( *p );
+    while( ++p != q )
+        res = f2( res, f1( *p ) );*/
+
+    auto res = f1( *p );
+    ++p;
+    for ( std::size_t t = 0; t < threads; ++t )
+    {
+        std::size_t counter = 0;
+        for ( ; counter < cnt_per_thrd && p != q; ++p, ++counter )
+        {
+            res = f2( res, f1( *p ) );
+        }
+    }
+
+    for ( ; p != q; ++p )
+    {
+        res = f2( res, f1( *p ) );
+    }
+
+    return res;
 }
 
 int main()
@@ -24,6 +47,7 @@ int main()
         ,   3
     );
 
+    std::cout << sum << std::endl;
     assert( sum == 55 );
 
     // проверка наличия чётных чисел в четыре потока
@@ -35,12 +59,13 @@ int main()
         ,   4
     );
 
+    std::cout << has_even << std::endl;
     assert( has_even == true );
 
     std::vector< std::string > const v = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11" };
     for ( std::size_t i = 1; i <= v.size(); ++i )
     {
-        auto sum = map_reduce(
+        auto ssum = map_reduce(
                 v.begin()
             ,   v.end()
             ,   []( std::string i ){ return i; }
@@ -48,7 +73,8 @@ int main()
             ,   i
         );
 
-        assert( sum == "1234567891011" );
+        std::cout << ssum << std::endl;
+        assert( ssum == "1234567891011" );
     }
 
     return 0;
