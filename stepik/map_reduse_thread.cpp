5f6e2ac9906127540963597de649ce386eb71214
Georgiy Kulak <kulakgeorgij@gmail.com>
2021-01-30 17:02:32 +0200

    Stepik: Add solution for map_reduce with thread

---
+++ stepik/map_reduse_thread.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <cassert>
+#include <list>
+#include <thread>
+
+#include <mutex>
+#include <vector>
+#include <type_traits>
+
+// DON'T FORGET TO LINK "-lpthread" !!!
+
+std::mutex g_mutex;
+
+template< typename I, typename UF, typename BF >
+using MapReduceRetT =
+    decltype(
+        std::declval< BF >()(
+                std::declval< UF >()( *std::declval< I >() )
+            ,   std::declval< UF >()( *std::declval< I >() )
+        )
+    );
+
+template< typename BIterator, typename UnaryF, typename BinaryF >
+MapReduceRetT< BIterator, UnaryF, BinaryF > map_reduce (
+        BIterator p
+    ,   BIterator q
+    ,   UnaryF f1
+    ,   BinaryF f2
+    ,   size_t threads
+)
+{
+    using RetT = MapReduceRetT< BIterator, UnaryF, BinaryF >;
+
+    assert( threads );
+    
+    auto length = std::distance( p, q );
+
+    assert( length );
+
+    std::size_t const cnt_per_thrd = length / threads;
+
+    RetT res{};
+
+    auto lambda =
+        [ &f1, &f2 ]( BIterator p, BIterator q, RetT & currRes )
+        {
+            std::lock_guard< std::mutex > l_guard( g_mutex );
+            currRes = f2( currRes, f1( *p ) );
+            while ( ++p != q )
+                currRes = f2( currRes, f1( *p ) );
+        };
+
+    std::vector< std::thread > resVector;
+    resVector.reserve( threads );
+    
+    for ( std::size_t t = 0; t < threads; ++t )
+    {
+        std::size_t counter = 0;
+        
+        auto it = p;
+        if ( t + 1 != threads )
+            for ( ; counter < cnt_per_thrd && it != q; ++it, ++counter ) ;
+        else
+            for ( ; it != q; ++it ) ;
+
+        resVector.emplace_back(
+            std::thread( lambda, p, it, std::ref( res ) )
+        );
+
+        p = it;
+    }
+
+    for ( auto & fut : resVector )
+        fut.join();
+
+    return res;
+}
+
+int main()
+{
+    std::list< int > const l1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    
+    // параллельное суммирование в 3 потока
+    auto sum = map_reduce(
+            l1.begin()
+        ,   l1.end()
+        ,   []( int i ){ return i; }
+        ,   std::plus< int >()
+        ,   3
+    );
+
+    assert( sum == 55 );
+
+    // проверка наличия чётных чисел в четыре потока
+    auto has_even = map_reduce(
+            l1.begin()
+        ,   l1.end()
+        ,   []( int i ){ return i % 2 == 0; }
+        ,   std::logical_or< bool >()
+        ,   4
+    );
+
+    assert( has_even == true );
+
+    std::vector< std::string > const v1 =
+        { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11" };
+    for ( std::size_t j = 1; j <= v1.size(); ++j )
+    {
+        auto ssum = map_reduce(
+                v1.begin()
+            ,   v1.end()
+            ,   []( std::string s ){ return s; }
+            ,   std::plus< std::string >()
+            ,   j
+        );
+
+        std::cout << ssum << std::endl;
+        //assert( ssum == "1234567891011" );
+    }
+
+    std::list< int > const l2 =
+        { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+
+    for ( int i = 1; i < 20; ++i )
+    {
+        auto ssum = map_reduce(
+                l2.begin()
+            ,   l2.end()
+            ,   []( int j ){ return j; }
+            ,   std::plus< int >()
+            ,   i
+        );
+
+        assert( ssum == 190 );
+    }
+
+    std::vector< std::string > const v2 =
+        { "multithread", "and", "async", "in", "C++", "is", "total", "sh!t" };
+
+    // If "i <= v2.size()": on Stepik OK, there NOT OK
+    for ( std::size_t i = 1; i <= 8; ++i )
+    {    
+        auto size_sum = map_reduce(
+                v2.begin()
+            ,   v2.end()
+            ,   []( std::string s ){ return s.size(); }
+            ,   std::plus< std::size_t >()
+            ,   i
+        );
+
+        assert( size_sum == 35 );
+    }
+
+    return 0;
+}
